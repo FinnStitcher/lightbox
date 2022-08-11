fa82528b9defef5185be98851728614c6ff72474
Finn Stitcher <foundfootage@protonmail.com>
2022-08-11 14:57:28 -0700

    homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User, Comment} = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['title', 'text', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));
+
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
