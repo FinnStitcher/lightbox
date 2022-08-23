74beabd1629f5cc82f1a65c485130ce118be6b15
Finn Stitcher <foundfootage@protonmail.com>
2022-08-23 16:12:43 -0700

    edit template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 const {Post, User, Comment} = require('../models');
 const checkAuth = require('../utils/auth');
 
+router.get('/login', (req, res) => {
+    res.render('login', { loggedIn: req.session.loggedIn });
+});
+
+// if user is logged in, send them to the logout page
+// else, send them to the "you need to be logged in" page
+router.get('/logout', (req, res) => {
+    res.render('logout', { loggedIn: req.session.loggedIn });
+});
+
+router.get('/unauthorized', (req, res) => {
+    res.render('redirect-to-login', {loggedIn: false});
+});
+
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'title', 'text', 'created_at'],
@@ -66,16 +80,6 @@ router.get('/posts/:id', (req, res) => {
     })
 });
 
-router.get('/login', (req, res) => {
-    res.render('login', { loggedIn: req.session.loggedIn });
-});
-
-// if user is logged in, send them to the logout page
-// else, send them to the "you need to be logged in" page
-router.get('/logout', (req, res) => {
-    res.render('logout', { loggedIn: req.session.loggedIn });
-});
-
 router.get('/dashboard', checkAuth, (req, res) => {
     User.findOne({
         where: {
@@ -113,6 +117,38 @@ router.get('/dashboard', checkAuth, (req, res) => {
 
 router.get('/create', (req, res) => {
     res.render('new-post', { loggedIn: req.session.loggedIn });
+});
+
+router.get('/edit/:id', checkAuth, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'text'],
+        include: [
+            {
+                model: User,
+                attributes: ['id']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'No post with that ID was found.'});
+        } else {
+            const post = dbPostData.get({plain: true});
+
+            // adding a value that identifies if the user made this post
+            // makes sure you can't edit 
+            post.belongsToUser = post.user.id === req.session.user_id;
+
+            res.render('edit-post', {post, loggedIn: req.session.loggedIn});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 module.exports = router;
